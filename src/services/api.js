34c4ea977cbbfe50c63310e2d10a7abf7441400a
Mirian <miriancordibelinogueira@gmail.com>
2022-08-05 19:06:46 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
-export async function getCategories() {
-  // Implemente aqui
-}
+export const getCategories = async () => {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await response.json();
+  return categories;
+};
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-}
+export const getProductsFromCategoryAndQuery = async (categoryId, query) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const products = await response.json();
+  return products;
+};
