cf94f55e81aacdb4d8405b2f4a2e1973fcfe431b
Ana <anactc2208@gmail.com>
2022-08-06 08:28:19 -0300

    requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    listCategories: [],
+  };
+
+  componentDidMount() {
+    this.createListCategories();
+  }
+
+  async createListCategories() {
+    const categories = await getCategories();
+    this.setState({
+      listCategories: categories,
+    });
+  }
+
   render() {
+    const { listCategories } = this.state;
+
     return (
       <div className="search">
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho de compras</Link>
@@ -10,6 +28,16 @@ class Home extends Component {
         <div data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </div>
+        Categorias:
+        {listCategories.map((category) => (
+          <label htmlFor="categories" key={ category.id } data-testid="category">
+            {category.name}
+            <input
+              type="radio"
+              name="category"
+            />
+          </label>
+        ))}
       </div>
     );
   }
